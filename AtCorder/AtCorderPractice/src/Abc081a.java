95c6d3e57618382cd9b0db044958276a11092ba6
matsubara999 <101336669+matsubara999@users.noreply.github.com>
2022-03-11 20:41:46 +0900

    変更

--- AtCorder/AtCorderPractice/src/Abc081a.java
+++ AtCorder/AtCorderPractice/src/Abc081a.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Abc081a {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String s = scan.nextLine();
-		
-		int sum = 0;
-		char[] chars = s.toCharArray();
-		for(int i = 0; i < chars.length; i++) {
-			sum += Character.getNumericValue(chars[i]); 
+		try (Scanner scan = new Scanner(System.in)) {
+			String s = scan.nextLine();
+
+			int sum = 0;
+			char[] chars = s.toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				sum += Character.getNumericValue(chars[i]);
+			}
+
+			System.out.println(sum);
 		}
-		
-		System.out.println(sum);
 	}
 }
