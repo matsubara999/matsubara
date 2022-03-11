c529f933c774c6567969548d0a247edc44a46206
matsubara999 <101336669+matsubara999@users.noreply.github.com>
2022-03-11 20:45:42 +0900

    change

--- AtCorder/AtCorderPractice/src/Abc086a.java
+++ AtCorder/AtCorderPractice/src/Abc086a.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class Abc086a{
-	public static void mai1n(String[] args){
-		Scanner scan = new Scanner(System.in);
-		int a =  scan.nextInt();
-		int b =  scan.nextInt();
-      
-		if(a*b%2 == 0) {
-			System.out.println("Even");
-		}else {
-			System.out.println("Odd");
-		};
-		
+public class Abc086a {
+	public static void man(String[] args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			if (a * b % 2 == 0) {
+				System.out.println("Even");
+			} else {
+				System.out.println("Odd");
+			}
+		}
+
 	}
 }
